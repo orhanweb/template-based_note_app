0ea2b65eaf4b5e9793bfbdfe42706dd35e56e31b
Orhan-Kahraman <herorhan999@gmail.com>
2023-04-26 23:48:43 +0300

    adding templates part_1

---
+++ lib/Home/View/HomeSubViews/templates_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TemplatesView extends StatelessWidget {
+  const TemplatesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
