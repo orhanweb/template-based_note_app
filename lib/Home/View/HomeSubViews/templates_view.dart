2f832a58ffa407a144d4a2aae952018ad371a414
Orhan-Kahraman <herorhan999@gmail.com>
2023-04-28 23:30:10 +0300

    adding template pages part 2

--- lib/Home/View/HomeSubViews/templates_view.dart
+++ lib/Home/View/HomeSubViews/templates_view.dart
@@ -1,16 +1,15 @@
 import 'package:ekin_app/Product/Widgets/ExpandableFabWidget/expandable_fab.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 
 class TemplatesView extends StatelessWidget {
   const TemplatesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: Center(
           child: Text("This area is Templates area"),
         ),
-        floatingActionButton: const CExpandableFabWidget());
+        floatingActionButton: CExpandableFabWidget());
   }
 }
